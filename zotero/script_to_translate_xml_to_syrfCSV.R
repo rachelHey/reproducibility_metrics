f9a268af8dbf9b1c084028bce98c6000ff287185
Rachel Heyard <rachel.heyard@uzh.ch>
2024-05-13 12:29:01 +0200

    de-duplication for syrf

--- zotero/script_to_translate_xml_to_syrfCSV.R
+++ zotero/script_to_translate_xml_to_syrfCSV.R
@@ -33,6 +33,19 @@ data_ebsco <- read_xml("zotero/ebsco_20240513.xml")
 # Merge them together:
 all_methodological_papers <- data_scopus %>%
   bind_rows(data_ebsco)
+
+# De-duplicate using DOI, if it is not NA
+## 1. extract the ones without a DOI
+all_methodological_papers <- all_methodological_papers %>%
+  filter(is.na(doi)) %>%
+  ## 2. Paste them together with the once with a DOI, after de-duplication:
+  bind_rows(
+    all_methodological_papers %>%
+      filter(!is.na(doi)) %>%
+      group_by(doi) %>% slice(1)
+  ) %>%
+  arrange(title)
+
 # Save as CSV for SYRF:
 write_for_syrf(all_methodological_papers,
                filename="all_methodological_papers_20240513.csv")
